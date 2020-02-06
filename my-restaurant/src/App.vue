5956bcb09582e7fb9bb90cd5997d9256e19e0928
Daniel B <danielb.alzate1@gmail.com>
2020-02-06 02:22:03 -0500

    Implementación SweetAlert2

--- my-restaurant/src/App.vue
+++ my-restaurant/src/App.vue
@@ -6,7 +6,7 @@
 			<router-link to="/about">Acerca de</router-link>
 		</div>
 		<router-view />
-		<progress :value="UploadValue" max="100" id="uploader">0%</progress>
+		<progress :value="uploadValue" max="100" id="uploader">0%</progress>
 		<br />
 		<input type="file" @change="onFileSelected" />
 		<br />
@@ -15,7 +15,60 @@
 		<img width="40%" :src="this.picture" />
 	</div>
 </template>
+<script>
+import firebase from "firebase";
+import Swal from "sweetalert2";
 
+export default {
+	name: "app",
+	data() {
+		return {
+			selectedFile: null,
+			uploadValue: 0,
+			picture: null
+		};
+	},
+	methods: {
+		onFileSelected(event) {
+			this.selectedFile = event.target.files[0];
+		},
+		onUpload() {
+			const storageRef = firebase.storage().ref(`/img/main-course/${this.selectedFile.name}`);
+			const task = storageRef.put(this.selectedFile);
+			const Swal = require("sweetalert2");
+			task.on(
+				"state_changed",
+				(snapshot) => {
+					/* let porcentaje = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+					this.uploadValue = porcentaje; */
+				},
+				(error) => {
+					console.log("error", error.message),
+						() => {
+							this.uploadValue = 100;
+
+							task.snapshot.ref.getDownloadURL().then((url) => {
+								this.picture = url;
+								console.log(this.picture);
+							});
+						};
+				},
+				() => {
+					Swal.fire({
+						icon: "success",
+						title: "Se ha subido la imagen correctamente.",
+						showConfirmButton: false,
+						timer: 1000,
+						onClose: () => {
+							location.reload();
+						}
+					});
+				}
+			);
+		}
+	}
+};
+</script>
 <style>
 #app {
 	font-family: "Avenir", Helvetica, Arial, sans-serif;
